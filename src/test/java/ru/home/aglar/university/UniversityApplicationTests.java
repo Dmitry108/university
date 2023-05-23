10442facf9ba53521b0ba575ef9cbadedea74abb
Dmitry Babanov <babnov1984@mail.ru>
2023-05-23 12:31:09 +0300

    init project

---
+++ src/test/java/ru/home/aglar/university/UniversityApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.home.aglar.university;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UniversityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
